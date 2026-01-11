9b88e983a4bbff2e26d42773ca66f69e2ff35ca0
renny <meow3199@gmail.com>
2026-01-11 14:22:32 +0800

    Implement methods in InMemoryUserRepository

--- src/InMemoryUserRepository.java
+++ src/InMemoryUserRepository.java
@@ -6,8 +6,27 @@ public class InMemoryUserRepository implements UserRepository{
     Map<String,User> data = new HashMap<>();
 
     @Override
+    public User findByAccount(String userAccount){
+        return data.get(userAccount);
+    }
+
+    @Override
+    public boolean existsByAccount(String userAccount){
+        return data.get(userAccount) != null // 或是 return data.containsKey(userAccount);
+    }
+
+    @Override
     public void save(User user){
         data.put(user.getUserAccount(),user);
+    }
+
+    @Override
+    public boolean deleteByAccount(String userAccount){
+        if(!data.containsKey(userAccount)){
+            return false;
+        }
+        data.remove(userAccount);
+        return true;
     }
 
 }
